e13ec3888975512ed124b7ed63dacd3d5d2ac77c
Sang <changocodon@gmail.com>
2017-03-21 09:58:03 +0700

    add feature update
    update quote and name by id

--- Nodejs/public/main.js
+++ Nodejs/public/main.js
@@ -1,15 +1,32 @@
 function editSQuote(i) {
-    var author = document.getElementById("name"+i);
-    var quote = document.getElementById("quote"+i);
-    
+    var author = document.getElementById("name" + i);
+    var quote = document.getElementById("quote" + i);
+    var quoteId = document.getElementById("quoteId" + i);
+
     document.getElementById("author").value = author.textContent;
     document.getElementById("quoteOfAuthor").value = quote.textContent;
-    alert(author.textContent +': '+ quote.textContent);
+    document.getElementById("quoteId").innerHTML = quoteId.textContent;
+    alert(author.textContent + ': ' + quote.textContent + quoteId.textContent);
 
 }
 
 function deleteSQuote(i) {
-    var author = document.getElementById("name"+i);
-    var quote = document.getElementById("quote"+i);
-    alert(author.textContent +': '+ quote.textContent);
+    var author = document.getElementById("name" + i);
+    var quote = document.getElementById("quote" + i);
+    alert(author.textContent + ': ' + quote.textContent);
+}
+
+function updateSQuote() {
+    var author = document.getElementById("author").value;
+    var quoteOfAuthor = document.getElementById("quoteOfAuthor").value;
+    var quoteId = document.getElementById("quoteId").innerHTML;
+     fetch('/up', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            'name':  author,
+            'quote': quoteOfAuthor,
+            '_id': quoteId
+        })
+    })
 }
--- Nodejs/server.js
+++ Nodejs/server.js
@@ -2,32 +2,58 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-app.set('view engine', 'ejs')
+const port = 8080;
 var db;
+
+app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+//recieved data from UI by put method
+app.use(bodyParser.json());
 
+// set data connection string and server listening
 MongoClient.connect('mongodb://sang:1902@ds123930.mlab.com:23930/star-war-quotes', (err, database) => {
     if (err) return console.log(err)
     db = database;
-    app.listen(3000, () => {
+    app.listen(port, () => {
         console.log('listening on 3000');
     });
 });
- 
+
+//Save data from UI
 app.post('/', (req, res) => {
     db.collection('quotes').save(req.body, (err, result) => {
         if (err) return console.log(err)
-
         console.log('saved to database');
         res.redirect('/');
     });
 });
 
+//Show list data in mongodb
 app.get('/', (req, res) => {
+    console.log('sang');
     db.collection('quotes').find().toArray((err, result) => {
         if (err) return console.log(err)
+        //console.log('truung',result);
         // renders index.ejs
         res.render('index.ejs', { quotes: result })
     });
+    console.log('end=sang');
+});
+
+//UPDATE data from UI
+app.put('/up', (req, res) => {
+    //console.log(req.body._id);
+    db.collection('quotes').findOneAndUpdate({ _id: req.body._id },
+        {
+            $set: {
+                name: req.body.name,
+                quote: req.body.quote
+            }
+        }, { 
+            new: true
+        }).then(function(result, err){
+            console.log('trung',result);
+        });
+        
 });
